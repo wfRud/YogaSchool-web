b983c7c1ccd08ddbcdb0adbc69d22427bd220dc4
Filip Rudek <rudek.filip@gmail.com>
2019-09-24 14:25:49 +0200

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+import '../scss/style.scss';
+import '../images/hero-img.jpg';
+import '../images/about-img.jpg';
+import '../images/gallery(1).jpg';
+import '../images/gallery(2).jpg';
+import '../images/gallery(3).jpg';
+import '../images/gallery(4).jpg';
+import '../images/gallery(5).jpg';
+import '../images/gallery(6).jpg';
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
