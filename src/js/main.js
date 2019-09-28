c581523023a89bb11c1ce7e1db91f75c5e16f9e8
Filip Rudek <rudek.filip@gmail.com>
2019-09-28 16:49:01 +0200

    Added RWD

--- src/js/main.js
+++ src/js/main.js
@@ -7,7 +7,15 @@ import '../images/gallery(3).jpg';
 import '../images/gallery(4).jpg';
 import '../images/gallery(5).jpg';
 import '../images/gallery(6).jpg';
+// cSpell:disable
 import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/regular';
 import '@fortawesome/fontawesome-free/js/brands';
+
+
+const navigation = document.getElementById('navigation');
+
+document.addEventListener('click', () => {
+    navigation.classList.toggle('active');
+});
