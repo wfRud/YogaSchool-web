c2146be9353209c9d28cd36973cb04e5fa5db688
Filip Rudek <rudek.filip@gmail.com>
2021-10-07 21:49:51 +0200

    Update dependencies

--- src/js/calendar.js
+++ src/js/calendar.js
@@ -1,103 +1,101 @@
-import {
-    Calendar
-} from '@fullcalendar/core';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import '@fullcalendar/core/main.css';
-import '@fullcalendar/daygrid/main.css';
-import '../scss/calendarStyles.scss';
+import { Calendar } from "@fullcalendar/core";
+import dayGridPlugin from "@fullcalendar/daygrid";
+// import '@fullcalendar/core/main.css';
+import "@fullcalendar/daygrid/main.css";
+import "../scss/calendarStyles.scss";
 
-document.addEventListener('DOMContentLoaded', function () {
-    var calendarEl = document.getElementById('calendar');
+document.addEventListener("DOMContentLoaded", function () {
+  var calendarEl = document.getElementById("calendar");
 
-    var calendar = new Calendar(calendarEl, {
-        plugins: [dayGridPlugin],
-        height: 400,
-        handleWindowResize: true,
-        header: {
-            left: 'title',
-            center: '',
-            right: 'today prev, next',
-        },
-        events: [{
-                title: 'Yoga Classes',
-                start: '2019-09-02T10:30:00',
-                allDay: true,
-                description: 'Activity',
-                backgroundColor: '#8565c4',
-            },
-            {
-                title: 'Meditation',
-                start: '2019-09-06T10:30:00',
-                allDay: true,
-                description: 'Activity',
-                backgroundColor: '#77dd77',
-            },
-            {
-                title: 'Yoga Classes',
-                start: '2019-09-09T10:30:00',
-                allDay: true,
-                description: 'Activity',
-                backgroundColor: '#8565c4',
-            },
-            {
-                title: 'Fitness',
-                start: '2019-09-12T10:30:00',
-                allDay: true,
-                description: 'Activity',
-                backgroundColor: '#cb99c9',
-            },
-            {
-                title: 'Yoga Classes',
-                start: '2019-09-16T10:30:00',
-                allDay: true,
-                description: 'Activity',
-                backgroundColor: '#8565c4',
-            },
-            {
-                title: 'Meditation',
-                start: '2019-09-17T10:30:00',
-                allDay: true,
-                description: 'Passive',
-                backgroundColor: '#77dd77',
-            },
-            {
-                title: 'Yoga Classes',
-                start: '2019-09-23T10:30:00',
-                allDay: true,
-                description: 'Activity',
-                backgroundColor: '#8565c4',
-            },
-            {
-                title: 'Yoga Classes',
-                start: '2019-09-26T10:30:00',
-                allDay: true,
-                description: 'Activity',
-                backgroundColor: '#8565c4',
-            },
-            {
-                title: 'Yoga Classes',
-                start: '2019-09-30T10:30:00',
-                allDay: true,
-                description: 'Activity',
-                backgroundColor: '#8565c4',
-            },
-            {
-                title: 'Indyvidual Classes',
-                start: '2019-10-05T10:30:00',
-                allDay: true,
-                description: 'Activity',
-                backgroundColor: '#cb99c9',
-            },
-            {
-                title: 'Fitness',
-                start: '2019-10-20T10:30:00',
-                allDay: true,
-                description: 'Activity',
-                backgroundColor: '#cb99c9',
-            },
+  var calendar = new Calendar(calendarEl, {
+    plugins: [dayGridPlugin],
+    height: 400,
+    handleWindowResize: true,
+    header: {
+      left: "title",
+      center: "",
+      right: "today prev, next",
+    },
+    events: [
+      {
+        title: "Yoga Classes",
+        start: "2019-09-02T10:30:00",
+        allDay: true,
+        description: "Activity",
+        backgroundColor: "#8565c4",
+      },
+      {
+        title: "Meditation",
+        start: "2019-09-06T10:30:00",
+        allDay: true,
+        description: "Activity",
+        backgroundColor: "#77dd77",
+      },
+      {
+        title: "Yoga Classes",
+        start: "2019-09-09T10:30:00",
+        allDay: true,
+        description: "Activity",
+        backgroundColor: "#8565c4",
+      },
+      {
+        title: "Fitness",
+        start: "2019-09-12T10:30:00",
+        allDay: true,
+        description: "Activity",
+        backgroundColor: "#cb99c9",
+      },
+      {
+        title: "Yoga Classes",
+        start: "2019-09-16T10:30:00",
+        allDay: true,
+        description: "Activity",
+        backgroundColor: "#8565c4",
+      },
+      {
+        title: "Meditation",
+        start: "2019-09-17T10:30:00",
+        allDay: true,
+        description: "Passive",
+        backgroundColor: "#77dd77",
+      },
+      {
+        title: "Yoga Classes",
+        start: "2019-09-23T10:30:00",
+        allDay: true,
+        description: "Activity",
+        backgroundColor: "#8565c4",
+      },
+      {
+        title: "Yoga Classes",
+        start: "2019-09-26T10:30:00",
+        allDay: true,
+        description: "Activity",
+        backgroundColor: "#8565c4",
+      },
+      {
+        title: "Yoga Classes",
+        start: "2019-09-30T10:30:00",
+        allDay: true,
+        description: "Activity",
+        backgroundColor: "#8565c4",
+      },
+      {
+        title: "Indyvidual Classes",
+        start: "2019-10-05T10:30:00",
+        allDay: true,
+        description: "Activity",
+        backgroundColor: "#cb99c9",
+      },
+      {
+        title: "Fitness",
+        start: "2019-10-20T10:30:00",
+        allDay: true,
+        description: "Activity",
+        backgroundColor: "#cb99c9",
+      },
+    ],
+  });
 
-        ]
-    });
-
-    calendar.render();
+  calendar.render();
 });
--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,45 +1,44 @@
-import {
-    setTimeout
-} from "timers";
+// import {
+//     setTimeout
+// } from "timers";
 
-const navigation = document.getElementById('navigation');
-const hamburger = document.querySelector('.hamburger');
-const dotNav = navigation.querySelector('.dot-nav');
-const dotNavLink = navigation.querySelectorAll('.dot-nav__link');
+const navigation = document.getElementById("navigation");
+const hamburger = document.querySelector(".hamburger");
+const dotNav = navigation.querySelector(".dot-nav");
+const dotNavLink = navigation.querySelectorAll(".dot-nav__link");
 let flag = false;
-
 
 // Navigation Mobile Screen
 const menuLinkClick = () => {
-    hamburger.classList.toggle('active');
-    dotNav.classList.toggle('active');
+  hamburger.classList.toggle("active");
+  dotNav.classList.toggle("active");
 };
 
 const handleClick = () => {
-    hamburger.classList.toggle('active');
-    dotNav.classList.toggle('active');
+  hamburger.classList.toggle("active");
+  dotNav.classList.toggle("active");
 };
 
-hamburger.addEventListener('click', handleClick);
-dotNavLink.forEach(linkElement => {
-    linkElement.addEventListener('click', menuLinkClick);
+hamburger.addEventListener("click", handleClick);
+dotNavLink.forEach((linkElement) => {
+  linkElement.addEventListener("click", menuLinkClick);
 });
 
 // Desktop Navigation
 const showNavigate = () => {
-    if (window.innerWidth > 768 && (!flag)) {
-        dotNav.classList.add('active')
-        flag = !flag
-        if (dotNav.classList.contains('active') && flag) {
-            hideNavigate();
-        }
-    };
-}
+  if (window.innerWidth > 768 && !flag) {
+    dotNav.classList.add("active");
+    flag = !flag;
+    if (dotNav.classList.contains("active") && flag) {
+      hideNavigate();
+    }
+  }
+};
 const hideNavigate = () => {
-    setTimeout(() => {
-        dotNav.classList.remove('active');
-        flag = !flag;
-    }, 2500);
-}
+  setTimeout(() => {
+    dotNav.classList.remove("active");
+    flag = !flag;
+  }, 2500);
+};
 
-document.addEventListener('scroll', showNavigate);
+document.addEventListener("scroll", showNavigate);
